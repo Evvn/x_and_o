9b91224c4c8e204a8a01921f0674fa675b9b6748
Evan <evvn@outlook.com>
2018-07-04 10:42:09 +1000

    added starting icon selection logic

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
 console.log('x and o by the weeknd');
+
+var p1 = ''
+var p2 = ''
+var xBttn = document.querySelector('.x-bttn')
+var oBttn = document.querySelector('.o-bttn')
+var selectIcon = document.querySelector('.select-icon')
+var gameSquares = document.querySelectorAll('.square')
+
+// add event listeners to buttons
+selectIcon.addEventListener('click', function(e){
+  if (e.target.type === "submit") {
+    hideSelectIcon()
+    // set player 1 to selected icon
+    p1 = e.target.textContent
+    if (p1 === 'x') {
+      p2 = 'o'
+    } else {
+      p2 = 'x'
+    }
+  } else {
+    return;
+  }
+})
+
+// fade and then hide icon selection
+function hideSelectIcon() {
+  selectIcon.classList.add('fade')
+  setTimeout(function(){
+    selectIcon.classList.add('hide')
+  },1000)
+}
